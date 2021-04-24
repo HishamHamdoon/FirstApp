ea1e649d6e4bb30af20bc83d2de5fe7ee6000b1f
HishamHamdoon <41903899+HishamHamdoon@users.noreply.github.com>
2021-04-25 00:36:15 +0300

    Add files via upload
    done

--- delete_user.php
+++ delete_user.php
@@ -1,14 +1,10 @@
 <?php
-if (isset($submit)) {
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	$stmt =$con->prepare('INSERT INTO users 
-		( 
-		USERNAME, 
-		 PASSWORD,
-		  USER_GROUP,
-		   CREATED_AT ) VALUES(?,?,1,now())');
-	$stmt->execute(array($username,$password));
-	echo "user added succefully";
-
-} 
+session_start();
+include('include/connect.php');	
+$user_id = $_GET['user_id'];
+$stmt= $con->prepare("DELETE FROM users WHERE USER_ID = ?");
+$stmt->execute(array($user_id));
+echo "delete succefully";		
+// header("location : dashboard.php");
+// echo "hello";
+		?>
